417d84ed7e6c7301f96150c615deaddf5bc5fed0
Sam Windsor <121157983+windsywinds@users.noreply.github.com>
2023-12-16 00:33:39 +1300

    Added browser routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,35 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SearchProvider } from './pages/sam/searchContext';
 
-import { WebConfig } from "./pages/Admin";
-import { Home } from "./pages/sam/Homepage";
+import { WebConfig } from "./pages/services/Admin"; //needed for seeding the DB, add <WebConfig /> to use
+
+
+//Navigation pages flow required:
+//Homepage
+import { Home } from './pages/sam/Homepage';
+//Search results page
+import { Results } from './pages/sam/Results';
+//Property listing page
+
+//Booking page
+
 
 function App() {
   return (
     <div className="bg-white flex flex-col text-black font-normal font-inter h-screen w-screen items-center">
-      
-      <Home />
+      {/* Header goes here */}
+      <Router>
+        <SearchProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/results" element={<Results/>} />
+            {/* Page route for listings path="/listing/:id" element= */}
+            {/* Page route for bookings path="/booking/:id" element= */}
+          </Routes>
+        </SearchProvider>
+      </Router>
+      {/* Footer goes here */}
     </div>
   );
 }
