21399e61eaa71a97bcaf092b2e0a9f211d938fec
Nils Thevenard <nils.thevenard@gmail.com>
2023-12-18 18:05:16 +1300

    completed layout for booking page

--- client/src/pages/nils/Header.jsx
+++ client/src/pages/nils/Header.jsx
@@ -1,4 +1,4 @@
-import metroLogo from "../../assets/metro-logo.svg";
+import metroLogo from "../../assets/logo.jpg";
 import downArrow from "../../assets/down-arrow.svg";
 import phoneIcon from "../../assets/phone-icon.svg";
 import hamburger from "../../assets/hamburger.svg";
@@ -26,9 +26,9 @@ export const Header = () => {
   }, []);
 
   return (
-    <div className=" flex flex-col justify-between ">
+    <div className=" flex flex-col w-[80%]  justify-between ">
       {/* Phone view */}
-      <div className=" md:hidden w-screen flex flex-row justify-between ">
+      <div className=" md:hidden flex flex-row justify-between ">
         <img
           className="flex w-[4.75rem] pl-5 pt-2 pb-1 cursor-pointer"
           src={metroLogo}
@@ -71,23 +71,20 @@ export const Header = () => {
       )}
       {/* monitor view */}
 
-      <div className=" hidden md:flex w-screen flex-row  ">
+      <div className=" hidden md:flex  flex-row  justify-between ">
         <img
-          className="h-11 pl-[18vw] pt-2 pr-[7.8rem] cursor-pointer"
+          className="h-11  pt-2 pr-[7.8rem] cursor-pointer"
           src={metroLogo}
           onClick={() => navigate("/")}
         />
-        <div className=" text-[.687rem] font-normal font-['Inter'] flex flex-row items-center ">
+        <div className=" text-[.687rem] font-normal  flex flex-row items-center  ">
           <div className="pr-5">Rental Property Search</div>
           <div className="pr-5">Property Management Services</div>
           <div className="pr-5">About Us</div>
           <div className="pr-5">Contact Us</div>
-        </div>
+          <img className="w-5 pr-1" src={phoneIcon}></img>
 
-        <div className="flex flex-auto items-center">
-          <img className="w-4 pr-1" src={phoneIcon}></img>
-
-          <div className="text-[.687rem] pr-1">09 391 4642</div>
+          <div className="text-[.687rem] pr-1 font-bold">09 391 4642</div>
         </div>
       </div>
     </div>
--- client/src/pages/sam/Footer.jsx
+++ client/src/pages/sam/Footer.jsx
@@ -1,82 +1,109 @@
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useState, useRef } from 'react';
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useRef } from "react";
 
-import logo from '../../assets/metro-logo.svg'
-import phoneIcon from '../../assets/footer-phone-icon.svg'
-import emailIcon from '../../assets/email-icon.svg'
-import locationIcon from '../../assets/location-icon.svg'
+import logo from "../../assets/metro-logo.svg";
+import phoneIcon from "../../assets/footer-phone-icon.svg";
+import emailIcon from "../../assets/email-icon.svg";
+import locationIcon from "../../assets/location-icon.svg";
 
 export const Footer = () => {
-    const navigate = useNavigate(); //to use navigation
-    const location = useLocation();
-    const [message, setMessage] = useState(false)
-    const nameRef = useRef(null);
-    const emailRef = useRef(null);
-    const subjectRef = useRef(null);
-    const messageRef = useRef(null);
+  const navigate = useNavigate(); //to use navigation
+  const location = useLocation();
+  const [message, setMessage] = useState(false);
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const subjectRef = useRef(null);
+  const messageRef = useRef(null);
 
-    const handleformSubmit = (e) => {
-        e.preventDefault()
-        setMessage(true)
-        nameRef.current.value = '';
-        emailRef.current.value = '';
-        subjectRef.current.value = '';
-        messageRef.current.value = '';
-    }
+  const handleformSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+    nameRef.current.value = "";
+    emailRef.current.value = "";
+    subjectRef.current.value = "";
+    messageRef.current.value = "";
+  };
 
-    return(
-        <div className="flex flex-col lg:flex-row w-full bg-black absolute bottom-0 left-0 right-0 py-12 text-white items-center md:justify-between">
-            
-            <div className="flex flex-row w-full px-12 lg:px-0 lg:w-1/2 lg:ml-6 items-center justify-between">
-                <div className="flex flex-col  space-y-2">
-                    <img className="bg-white" src={logo}></img>
+  return (
+    <div className="flex flex-col lg:flex-row w-full bg-black  bottom-0 left-0 right-0 py-12 text-white items-center md:justify-between">
+      {/* i have removed absolute as it was causing the footer to cover BookingPage. is that ok ? */}
+      <div className="flex flex-row w-full px-12 lg:px-0 lg:w-1/2 lg:ml-6 items-center justify-between">
+        <div className="flex flex-col  space-y-2">
+          <img className="bg-white" src={logo}></img>
 
-                    <div className="flex flex-row gap-4">
-                    <img className="w-6" src={phoneIcon} />
-                        <p>09 391 4642</p>
-                    </div>
+          <div className="flex flex-row gap-4">
+            <img className="w-6" src={phoneIcon} />
+            <p>09 391 4642</p>
+          </div>
 
-                    <div className="flex flex-row gap-4">
-                        <img className="w-6"  src={emailIcon} />
-                        <p>info@metronz.co.nz</p>
-                    </div>
+          <div className="flex flex-row gap-4">
+            <img className="w-6" src={emailIcon} />
+            <p>info@metronz.co.nz</p>
+          </div>
 
-                    <div className="flex flex-row gap-4">
-                        <img className="w-6"  src={locationIcon} />
-                        <div>
-                        <p>Level 33, 23-29</p>
-                        <p>Albert Street</p>
-                        <p>Auckland CBD</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-col text-center lg:text-left space-y-4">
-                    <h4 className="flex flex-col text-lg md:text-xl font-bold underline">Quick Links</h4>
-                    <p className="cursor-pointer" onClick={() => navigate('/home')} >Home</p>
-                    <a href="#">Services</a>
-                    <a href="#">Tenants</a>
-                    <a href="#">News</a>
-                </div>
+          <div className="flex flex-row gap-4">
+            <img className="w-6" src={locationIcon} />
+            <div>
+              <p>Level 33, 23-29</p>
+              <p>Albert Street</p>
+              <p>Auckland CBD</p>
             </div>
-
-            <form className="flex flex-col w-[90%] md:w-2/5 md:mr-6 items-center space-y-6"
-                    onSubmit={handleformSubmit}>
-                <h4 className="text-lg md:text-xl font-bold underline pt-8">Contact Us</h4>
-                <div className="flex flex-col space-y-4 w-full">
-                    <div className="flex flex-col md:flex-row w-full space-y-4 md:space-y-0 justify-between ">
-                        <input ref={nameRef} required className="bg-black md:w-[45%] placeholder-white border-b-2 focus:outline-none" placeholder="Your Name"></input>
-                        <input ref={emailRef} required className="bg-black md:w-[45%] placeholder-white border-b-2 focus:outline-none"  placeholder="Your Email"></input>
-                    </div>
-                    <input ref={subjectRef} required className="bg-black placeholder-white border-b-2 focus:outline-none"  placeholder="Subject"></input>
-                    <input ref={messageRef} required className="bg-black placeholder-white border-b-2 focus:outline-none"  placeholder="Message"></input>
-                </div>
-                <button className="border-2 border-white w-[65%] py-2">Send</button>
-                {message && 
-                <div>
-                    <p>Thanks for getting in touch! We'll respond shortly.</p>
-                    </div>
-                }
-            </form>
+          </div>
         </div>
-    )
-}
+        <div className="flex flex-col text-center lg:text-left space-y-4">
+          <h4 className="flex flex-col text-lg md:text-xl font-bold underline">
+            Quick Links
+          </h4>
+          <p className="cursor-pointer" onClick={() => navigate("/home")}>
+            Home
+          </p>
+          <a href="#">Services</a>
+          <a href="#">Tenants</a>
+          <a href="#">News</a>
+        </div>
+      </div>
+      <form
+        className="flex flex-col w-[90%] md:w-2/5 md:mr-6 items-center space-y-6"
+        onSubmit={handleformSubmit}
+      >
+        <h4 className="text-lg md:text-xl font-bold underline pt-8">
+          Contact Us
+        </h4>
+        <div className="flex flex-col space-y-4 w-full">
+          <div className="flex flex-col md:flex-row w-full space-y-4 md:space-y-0 justify-between ">
+            <input
+              ref={nameRef}
+              required
+              className="bg-black md:w-[45%] placeholder-white border-b-2 focus:outline-none"
+              placeholder="Your Name"
+            ></input>
+            <input
+              ref={emailRef}
+              required
+              className="bg-black md:w-[45%] placeholder-white border-b-2 focus:outline-none"
+              placeholder="Your Email"
+            ></input>
+          </div>
+          <input
+            ref={subjectRef}
+            required
+            className="bg-black placeholder-white border-b-2 focus:outline-none"
+            placeholder="Subject"
+          ></input>
+          <input
+            ref={messageRef}
+            required
+            className="bg-black placeholder-white border-b-2 focus:outline-none"
+            placeholder="Message"
+          ></input>
+        </div>
+        <button className="border-2 border-white w-[65%] py-2">Send</button>
+        {message && (
+          <div>
+            <p>Thanks for getting in touch! We'll respond shortly.</p>
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
